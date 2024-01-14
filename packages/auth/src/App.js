5205854f6fac4bc8f40c9426f3426b10bfd1ba3b
elchinovzla <sonnyng.vzla@gmail.com>
2024-01-14 09:12:39 -0700

    create sign in and sign up pages for auth

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Switch, Route, Router } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core';
 
+import SignIn from './components/Signin';
+import SignUp from './components/Signup';
+
 const generateClassName = createGenerateClassName({
   productionPrefix: 'au',
 });
@@ -11,7 +14,10 @@ export default ({ history }) => {
     <div>
       <StylesProvider generateClassName={generateClassName}>
         <Router history={history}>
-          <Switch></Switch>
+          <Switch>
+            <Route path="/auth/signin" component={SignIn} />
+            <Route path="/auth/signup" component={SignUp} />
+          </Switch>
         </Router>
       </StylesProvider>
     </div>
